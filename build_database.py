4b2b094360b94c99abab00b4e713a0dd7d41c388
Guido Meijer <gtmeijer@gmail.com>
2020-12-05 16:42:31 +0000

    Build word library database

---
+++ build_database.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  5 14:24:04 2020
+
+@author: Guido Meijer
+"""
+
+import pickle
+from functions import word_cleanup
+import datetime
+from biorxiv_retriever import BiorxivRetriever
+br = BiorxivRetriever()
+
+# Settings
+FROM_DATE = '2020-12-03'
+TO_DATE = '2020-12-02'
+
+# Load current word library
+word_library = pickle.load(open('word_library.obj', 'rb'))
+
+date_now = datetime.datetime.strptime(FROM_DATE, '%Y-%m-%d')
+while date_now >= datetime.datetime.strptime(TO_DATE, '%Y-%m-%d'):
+    print('Scraping papers from %s' % str(date_now.date()))
+
+    # Query papers of this date
+    papers = br.query('limit_from%%3A%s limit_to%%3A%s' % (str(date_now.date()),
+                                                           str(date_now.date())), full_text=False)
+
+    # Extract words
+    current_size = len(word_library)
+    for i in range(len(papers)):
+        new_words = word_cleanup(papers[i]['abstract'].split())
+        word_library = word_library | new_words
+
+    # Print result
+    print('Added %d new words to library of %d words' % (len(word_library) - current_size,
+                                                         len(word_library)))
+
+    # Save to disk
+    file_library = open('word_library.obj', 'wb')
+    pickle.dump(word_library, file_library)
+    file_library.close()
+
+    # Continue with day before
+    date_now = date_now - datetime.timedelta(1)
---
+++ functions.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  5 15:30:19 2020
+
+@author: Guido Meijer
+"""
+
+from string import punctuation
+
+
+def remove_punctuation(value):
+    punctuation_no_dash = punctuation.replace('-', '')
+    result = ""
+    for c in value:
+        if c not in punctuation_no_dash:
+            result += c
+    return result
+
+
+def word_cleanup(word_list):
+    clean_word_list = set()
+    for word in word_list:
+        clean_word = remove_punctuation(word)
+        if (clean_word.islower() is False) or (any(map(str.isdigit, clean_word))):
+            continue
+        clean_word_list.add(clean_word)
+    return clean_word_list
