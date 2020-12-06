e6f90a8e8d834bead7a7bf57d582b80a8d08ceab
Guido <guido.meijer@research.fchampalimaud.org>
2020-12-06 12:42:49 +0000

    Update library

--- build_library.py
+++ build_library.py
@@ -12,8 +12,8 @@ from biorxiv_retriever import BiorxivRetriever
 br = BiorxivRetriever()
 
 # Settings
-FROM_DATE = '2020-11-22'
-TO_DATE = '2020-11-01'
+FROM_DATE = '2020-11-06'
+TO_DATE = '2019-01-01'
 
 # Load current word library
 word_library = pickle.load(open('word_library.obj', 'rb'))
@@ -23,23 +23,30 @@ while date_now >= datetime.datetime.strptime(TO_DATE, '%Y-%m-%d'):
     print('Scraping papers from %s' % str(date_now.date()))
 
     # Query papers of this date
-    papers = br.query('limit_from%%3A%s limit_to%%3A%s' % (str(date_now.date()),
-                                                           str(date_now.date())), full_text=False)
+    try:
+        papers = br.query('limit_from%%3A%s limit_to%%3A%s' % (str(date_now.date()),
+                                                               str(date_now.date())),
+                          full_text=False)
 
-    # Extract words
-    current_size = len(word_library)
-    for i in range(len(papers)):
-        new_words = word_cleanup(papers[i]['abstract'].split())
-        word_library = word_library | new_words
+        # Extract words
+        current_size = len(word_library)
+        for i in range(len(papers)):
+            new_words = word_cleanup(papers[i]['abstract'].split())
+            word_library = word_library | new_words
 
-    # Print result
-    print('Added %d new words to library of %d words' % (len(word_library) - current_size,
-                                                         len(word_library)))
+        # Print result
+        print('\nAdded %d new words to library of %d words' % (len(word_library) - current_size,
+                                                               len(word_library)))
 
-    # Save to disk
-    file_library = open('word_library.obj', 'wb')
-    pickle.dump(word_library, file_library)
-    file_library.close()
+        # Save to disk
+        file_library = open('word_library.obj', 'wb')
+        pickle.dump(word_library, file_library)
+        file_library.close()
 
-    # Continue with day before
-    date_now = date_now - datetime.timedelta(1)
+        # Continue with day before
+        date_now = date_now - datetime.timedelta(1)
+
+    except Exception as error_message:
+       print(error_message)
+
+
