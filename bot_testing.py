6ca4f58e6d2f00aad5454e8a1e2dc698825b485a
Guido Meijer <gtmeijer@gmail.com>
2020-12-16 19:21:43 +0000

    Improved word cleanup

--- bot_testing.py
+++ bot_testing.py
@@ -22,6 +22,7 @@ papers = br.query('limit_from%%3A%s limit_to%%3A%s' % (str(today), str(today)), 
 # Extract words
 new_words = set()
 for i in range(len(papers)):
+    # these_words = papers[i]['abstract'].split()
     these_words = word_cleanup(papers[i]['abstract'].split())
     for j, word in enumerate(these_words):
         if word not in word_library:
--- functions.py
+++ functions.py
@@ -5,28 +5,28 @@ Created on Sat Dec  5 15:30:19 2020
 @author: Guido Meijer
 """
 
-from string import punctuation
 
-
-def remove_punctuation(value):
-    punctuation_no_dash = punctuation + '“”‘’'
-    punctuation_no_dash = punctuation_no_dash.replace('-', '')
-    result = ""
-    for c in value:
-        if c not in punctuation_no_dash:
-            result += c
-    return result
+def remove_punctuation(word):
+    punctuation = '.,()—–-;/?:′’“”\'\"'
+    try:
+        while word[0] in punctuation:
+            word = word[1:]
+        while word[-1] in punctuation:
+            word = word[:-1]
+    except:
+        word = ''
+    return word
 
 
 def word_cleanup(word_list):
+    punctuation = '.,()/;′:\'\"'
     clean_word_list = set()
     for word in word_list:
         clean_word = remove_punctuation(word)
         if ((clean_word.islower() is True) and not (any(map(str.isdigit, clean_word)))
-                and (clean_word[0] != '–') and (clean_word[-1] != '–')
-                and (clean_word[0] != '-') and (clean_word[-1] != '-')
                 and (clean_word.count('-') <= 1) and (clean_word.count('–') <= 1)
-                and (len(clean_word) >= 2)):
+                and (any(elem in punctuation for elem in clean_word) is False)
+                and (len(clean_word) > 3)):
             clean_word_list.add(clean_word)
     clean_word_list.difference_update([i for i in clean_word_list if i.startswith('http')])
     clean_word_list.difference_update([i for i in clean_word_list if i.startswith('www')])
