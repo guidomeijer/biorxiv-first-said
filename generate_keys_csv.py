c51ce9933a0c54b79736efee4310a081319a5395
Guido Meijer <gtmeijer@gmail.com>
2020-12-21 14:40:34 +0100

    Keys for where bot

--- generate_keys_csv.py
+++ generate_keys_csv.py
@@ -8,5 +8,5 @@ keys = pd.DataFrame(index=[0], data={'access_token': access_token,
                                      'access_token_secret': access_token_secret,
                                      'api_key': api_key,
                                      'api_key_secret': api_key_secret})
-keys.to_csv('keys.csv')
+keys.to_csv('where_keys.csv')
 print('Keys saved successfully')
